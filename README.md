7aed580282d2cd7f7bbc991e8f8344cd695be745
grvoyt <grvoyt@ya.ru>
2019-05-16 01:34:30 +0300

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+# Estismail api helper
+
+### Init class
+```php
+require 'estismail.php';
+
+use Grvoyt\Estismail\Estismail;
+
+$estismail = Estismail::init('api_key');
+```
+
+### Add email to list
+
+```php
+$fields = [
+    'list_id' => 11111,
+    'email' => 'admin@admin.ru',
+];
+
+//return email id inserted
+$email_id = $estismail->addListEmail($fields);
+```
+
+### Get emails from list id
+```php
+//required fields for this method
+$fields = [
+    'list_id' => 11111
+];
+
+$response = $estismail->getListEmails($fields);
+```
---
+++ estismail.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Grvoyt\Estismail;
+
+class Estismail
+{
+    private $url = 'https://v1.estismail.com/';
+    private $api_key;
+    private $list_id;
+    private $required_fields = [
+        'email.index' => [
+            'list_id'
+        ],
+        'email.add' => [
+            'list_id',
+            'email'
+        ],
+        'email.view' => [
+            'list_id'
+        ]
+    ];
+
+    /**
+     * Estismail constructor.
+     * @param string $api
+     */
+    public function __construct(string $api)
+    {
+        $this->api_key = $api;
+        return $this;
+    }
+
+    /**
+     * @param $api
+     * @return Estismail
+     */
+    public static function init($api)
+    {
+        return new self($api);
+    }
+
+    /**
+     * @param $params
+     * @return bool|string
+     */
+    public function getListEmails($params)
+    {
+        $url = $this->url.'mailer/emails';
+        return $this->checkRequireParams('email.index', $params)
+            ? $this->sendRequest('GET', $url, $params)
+            : 'no required field';
+    }
+
+    /**
+     * @param $params
+     * @return string
+     */
+    public function addListEmail($params)
+    {
+        $url = $this->url.'mailer/emails';
+
+        if( !$this->checkRequireParams('email.add', $params) ) {
+            return 'no required field';
+        }
+
+        $response = json_decode($this->sendRequest('POST', $url, $params), true);
+
+        return $response['id'];
+    }
+
+    /**
+     * @param $type
+     * @param array $params
+     * @return bool
+     */
+    protected function checkRequireParams($type,$params = [])
+    {
+        foreach($this->required_fields[$type] as $value) {
+            if( !isset($params[$value]) ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @param string $method
+     * @param $url
+     * @param array $data
+     * @return bool|string
+     */
+    protected function sendRequest($method = 'GET', $url, $data = [])
+    {
+        $ch = curl_init();
+        if($method == 'GET') {
+            $url .= '?'.http_build_query($data);
+        }
+        curl_setopt($ch, CURLOPT_URL, $url );
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        if($method !== 'GET') {
+            $type_option = ($method == 'POST') ? CURLOPT_POST : CURLOPT_CUSTOMREQUEST;
+            curl_setopt($ch, $type_option, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        }
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'X-Estis-Auth: '.$this->api_key
+        ));
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        return $response;
+    }
+}
